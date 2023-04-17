62cb8867c6c8afdc87cd7f45d97dd1e37cbe9fe0
rhviso <130929793+rhviso@users.noreply.github.com>
2023-04-16 16:19:34 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var password = "soon"
+
+function passcheck() {
+
+    if(document.getElementById('soon').value != password) {
+        alert('incorrect');
+    }
+
+    if(document.getElementById('soon').value != password) {
+        alert('iwelcome');
+    }
+}
